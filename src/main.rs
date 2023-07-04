84f707f1f640d196cfee9b5d1e8cb727ce2a6460
petrostrak <pit.trak@gmail.com>
2023-07-04 17:05:53 +0300

    return json from hello endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use rocket::routes;
+use rocket::serde::json::{json, Value};
 
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!\n"
+fn hello() -> Value {
+    json!("Hello, world!")
 }
 
 #[rocket::main]
