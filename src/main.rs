e28325e35b8430eaf081d646535e2b6be461bc26
petrostrak <pit.trak@gmail.com>
2023-07-05 16:47:10 +0300

    Move auth logic to auth module and refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,12 @@
-use base64::{engine::general_purpose, Engine};
 use rocket::{
-    http::Status,
-    request::{FromRequest, Outcome, Request},
     response::status,
     serde::json::{json, Value},
 };
 
 #[macro_use]
 extern crate rocket;
-extern crate base64;
-
-pub struct BasicAuth {
-    pub username: String,
-    pub password: String,
-}
-
-impl BasicAuth {
-    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
-        let split = header.split_whitespace().collect::<Vec<_>>();
-        if split.len() != 2 {
-            return None;
-        }
-        if split[0] != "Basic" {
-            return None;
-        }
-
-        Self::from_base64_encoded(split[1])
-    }
-
-    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
-        let decoded = general_purpose::STANDARD.decode(base64_string).ok()?;
-        let decoded_str = String::from_utf8(decoded).ok()?;
-        let split = decoded_str.split(":").collect::<Vec<_>>();
-
-        if split.len() != 2 {
-            return None;
-        }
-
-        let (username, password) = (split[0].to_string(), split[1].to_string());
-        Some(BasicAuth { username, password })
-    }
-}
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for BasicAuth {
-    type Error = ();
-
-    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let auth_header = request.headers().get_one("Authorization");
-        if let Some(auth_header) = auth_header {
-            if let Some(auth) = Self::from_authorization_header(auth_header) {
-                return Outcome::Success(auth);
-            }
-        }
-        Outcome::Failure((Status::Unauthorized, ()))
-    }
-}
+mod auth;
+use auth::BasicAuth;
 
 // curl http://127.0.0.1:8000/rustaceans -H 'Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
 #[get("/rustaceans")]
@@ -64,22 +15,22 @@ fn get_rustaceans(_auth: BasicAuth) -> Value {
 }
 
 #[get("/rustaceans/<id>")]
-fn view_rustacean(id: i32) -> Value {
+fn view_rustacean(id: i32, _auth: BasicAuth) -> Value {
     json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
 }
 
 #[post("/rustaceans", format = "json")]
-fn create_rustacean() -> Value {
+fn create_rustacean(_auth: BasicAuth) -> Value {
     json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
 }
 
 #[put("/rustaceans/<id>", format = "json")]
-fn update_rustacean(id: i32) -> Value {
+fn update_rustacean(id: i32, _auth: BasicAuth) -> Value {
     json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
 }
 
 #[delete("/rustaceans/<id>")]
-fn delete_rustacean(id: i32) -> status::NoContent {
+fn delete_rustacean(id: i32, _auth: BasicAuth) -> status::NoContent {
     status::NoContent
 }
 
