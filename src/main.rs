af4c884d5bad5e9cffb52e971501065d1fbfb1c1
petrostrak <pit.trak@gmail.com>
2023-07-06 12:36:50 +0300

    Implement getAll query with diesel

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 #[macro_use]
 extern crate rocket;
+#[macro_use]
 extern crate diesel;
 #[macro_use]
 extern crate rocket_sync_db_pools;
 
+use diesel::prelude::*;
+use models::Rustacean;
 use rocket::{
     response::status,
     serde::json::{json, Value},
 };
+use schema::rustaceans;
 
 mod auth;
+mod models;
+mod schema;
+
 use auth::BasicAuth;
 
-#[database("sqlite")]
+#[database("sqlite_path")]
 struct DB(diesel::SqliteConnection);
 
 // curl http://127.0.0.1:8000/rustaceans -H 'Authorization: Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='
 #[get("/rustaceans")]
-fn get_rustaceans(_auth: BasicAuth, _db: DB) -> Value {
-    json!([{"id": 1, "name": "John Doe" }, {"id": 2, "name": "Doe John" }])
+async fn get_rustaceans(_auth: BasicAuth, db: DB) -> Value {
+    db.run(|c| {
+        let result = rustaceans::table
+            .limit(100)
+            .load::<Rustacean>(c)
+            .expect("Failed to read rustaceans entries");
+        json!(result)
+    })
+    .await
 }
 
 #[get("/rustaceans/<id>")]
